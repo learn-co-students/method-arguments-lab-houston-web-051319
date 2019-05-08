272440b2ed15723cf12cbfb07ef3251536acd435
Resham Arora <resham.arora@gmail.com>
2019-05-08 21:43:21 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+define introduction(name)
+ puts "Hi, my name is #{name}."
+end
+
+define introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
